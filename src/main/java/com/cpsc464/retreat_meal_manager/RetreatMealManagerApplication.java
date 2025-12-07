fa43125493e0658df41d0488851b491065e7028f
Flores33 <ivan_flores33@yahoo.com>
2025-12-07 13:29:14 -0800

    Initial commit of Spring Boot + JavaFX project structure

---
+++ src/main/java/com/cpsc464/retreat_meal_manager/RetreatMealManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cpsc464.retreat_meal_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetreatMealManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetreatMealManagerApplication.class, args);
+	}
+
+}
